e3a3eed420f2a89b996aa4452c7a2da214695023
giriashutosh55 <156055943+giriashutosh55@users.noreply.github.com>
2024-01-10 12:30:14 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{   
+    int a,b,c;
+
+    printf("Enter a number a\n");
+    scanf("%d" , &a);
+    printf("Enter a number b\n");
+    scanf("%d" , &b);
+    printf("Enter a number c\n");
+    scanf("%d" , &c);
+
+    printf("The sum is %d\n" ,a+b+c);
+    printf("The sum is %d\n",a*b);
+
+    return 0;
+}
