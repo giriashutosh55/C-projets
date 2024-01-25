f183e9c7d905f169ae967bda85b3640f8c06e7c5
Ashutosh <156055943+giriashutosh55@users.noreply.github.com>
2024-01-25 19:30:31 +0530

    Add files via upload

---
+++ arrray.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int marks[3] = {2, 3, 5};
+
+    for (char i = 0; i< 2; i++)
+    {
+        
+        printf("the value of is %c\n", j);
+    }
+
+    return 0;
+}
